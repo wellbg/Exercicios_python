eec16662ae6b643b2d6df94368056399e04c4a5d
Wellington <wellington2906@gmail.com>
2020-04-03 02:40:21 -0300

    gera a tabuada

---
+++ tabuada.py
@@ -0,0 +1,11 @@
+#linhas e colunas da tabuada
+linha = 1
+coluna = 1
+#precisamos de dois loops
+while linha <= 10: #mantém a mesma linha até o valor chegar a 10
+    while coluna <= 10: #corre todas as colunas na primeira linha
+        print(linha*coluna, end = "\t") #end="\t" adiciona TAB no final
+        coluna += 1 #contador (coluna = coluna + 1)
+    linha += 1 #contador da linha
+    print() #quebra a linha e continua na de baixo
+    coluna = 1
